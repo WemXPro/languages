f5b61c1ad4443cadc02d3989c3fbebb882cfb962
GIGABAIT93 <59637348+GIGABAIT93@users.noreply.github.com>
2024-01-15 00:08:19 +0200

    update

--- en/client.php
+++ en/client.php
@@ -460,4 +460,7 @@
     'custom_options_desc' => 'Customize your service options for your package',
     'games' => 'Games',
     'node_full' => 'The node is full. Report this to an administrator',
+    'vat' => 'ПДВ',
+    'incl' => 'Incl.',
+    'excl' => 'Excl.'
 );
